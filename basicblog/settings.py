94144737fac09e0f20cf009c738d4b2a655d1497
Kaleigh Hutto <kaleighhutto@gmail.com>
2017-07-06 11:14:14 -0400

    allowed hosts      '127.0.0.1',     'kcode.pythonanywhere.com',

--- basicblog/settings.py
+++ basicblog/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'f6$0bv#f^_6%(y=01nvoo#&q&&0=iw!62xfuvgtg_jpjtcs7rp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'kcode.pythonanywhere.com',]
 
 
 # Application definition
